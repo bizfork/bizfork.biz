14e0d2d6bfa063830fa1bb8acd2f473ff07c8021
Maddy Guthridge <hello@maddyguthridge.com>
2024-07-19 13:58:40 +1000

    Basic app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+  <h1>Bizfork</h1>
+
+  <em>
+    We help businesses develop the problems they need for the solutions they
+    create.
+  </em>
+</div>
+
+<img src="/business-people-cheering.png" alt="business people cheering" />
+
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  h1 {
+    font-size: 10rem;
+  }
+
+  em {
+    font-size: 2rem;
+    padding: 30px 0px;
+  }
+
+  img {
+    width: 100%;
+  }
+</style>
